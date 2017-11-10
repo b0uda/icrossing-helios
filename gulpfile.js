1353c7f03b8458803f8c2a8a6541472c0ff69351
b0uda <benlahsen.bouda@gmail.com>
2017-11-10 17:32:59 +0000

    Change 50% content to i-crossing

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var server = require('gulp-server-livereload');
  
 gulp.task('webserver', function() {
-  gulp.src('./../html5up-helios 2/')
+  gulp.src('./../icrossing-helios/')
     .pipe(server({
       livereload: true,
       directoryListing: true,
